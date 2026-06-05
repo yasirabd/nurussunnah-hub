e9b166c4c2a942dce456c06cd08ea9c63eb33dfd
yasirabd <sir.yasirabd@gmail.com>
2026-06-05 11:06:53 +0700

    Fix Cloudflare OpenNext scripts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,28 +1,3 @@
-﻿import type { OpenNextConfig } from "@opennextjs/cloudflare";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  edgeExternals: ["node:crypto"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-} satisfies OpenNextConfig;
-
-export default config;
+export default defineCloudflareConfig();
